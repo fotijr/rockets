3a6c63e7eb61d70f9211f017d55dc412a01208a7
Dominic Foti <dominic@fotijr.com>
2018-08-10 08:41:46 -0400

    Added images

--- src/app/rocket.ts
+++ src/app/rocket.ts
@@ -1,7 +1,9 @@
 import { RocketConfig } from './rocketConfig';
+import { Image } from './image';
 
 export class Rocket {
     id: string;
     name: string;
     config: RocketConfig[];
+    img: Image;
   }

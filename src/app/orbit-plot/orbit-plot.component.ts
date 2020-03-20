4a83e9f4cec317085d01ca20eb75f8db455026a5
Dominic Foti <dominic@fotijr.com>
2020-03-19 23:03:46 -0400

    Added orbit labels

--- src/app/orbit-plot/orbit-plot.component.ts
+++ src/app/orbit-plot/orbit-plot.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit, HostBinding, Input } from '@angular/core';
+import { Component, HostBinding, Input } from '@angular/core';
+import { Orbit } from '../models';
 
 @Component({
   selector: 'rkt-orbit-plot',
   templateUrl: './orbit-plot.component.html',
   styleUrls: ['./orbit-plot.component.scss']
 })
-export class OrbitPlotComponent implements OnInit {
-  /** Image source path */
-  @Input() img: string;
+export class OrbitPlotComponent {
 
   /** Orbit in km */
-  @Input() orbit: number = 0;
+  @Input() orbit: Orbit = {altitude: 0, name: 'Loading'};
 
   /** Planet radius in km */
   @Input() planetSize: number = 0;
@@ -18,17 +17,17 @@ export class OrbitPlotComponent implements OnInit {
   @HostBinding('style.left')
   @HostBinding('style.right')
   public get horizontalOffset() {
-    return -((this.orbit * this.kmToPixelScale) * Math.PI) + 'px';
+    return -((this.orbit.altitude * this.kmToPixelScale) * Math.PI) + 'px';
   }
 
   @HostBinding('style.top')
   public get topOffset() {
-    return (-this.orbit * this.kmToPixelScale) + 'px';
+    return (-this.orbit.altitude * this.kmToPixelScale) + 'px';
   }
 
   @HostBinding('style.bottom')
   public get bottomOffset() {
-    return -(this.orbit * this.kmToPixelScale) + 'px';
+    return -(this.orbit.altitude * this.kmToPixelScale) + 'px';
   }
 
   private get kmToPixelScale(): number {
@@ -37,8 +36,5 @@ export class OrbitPlotComponent implements OnInit {
   }
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
